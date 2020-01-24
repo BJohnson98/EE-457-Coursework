7de68d512325b57e7255083898c4ab65c8ded6fb
Brandon Johnson <47310376+BJohnson98@users.noreply.github.com>
2020-01-24 13:09:56 -0600

    Update Electromechanical_System.m

--- Transient-Stability/HW1/Electromechanical_System.m
+++ Transient-Stability/HW1/Electromechanical_System.m
@@ -28,7 +28,7 @@ lambda0 = 0;                % Initial Flux Linkage (Wb-t)
 z0 = [v0;x0;lambda0];       % Initial-condition vector
 
 % exogenous input
-Vs = 5;     % Voltage at the source (V)
+Vs = 0;     % Voltage at the source (V)
 f = 0;      % Mechanical Input (N);
 u = [f;Vs]; % external force vector
 
@@ -40,22 +40,34 @@ T = [0 0.5]; % (s)
 
 % Plot results
 % speed
-subplot(3,1,1)
+subplot(4,1,1)
 plot(t,z(:,1))
 xlabel('t(s)');
 ylabel('v(m/s)');
 
 % position
-subplot(3,1,2)
+subplot(4,1,2)
 plot(t,z(:,2))
 xlabel('t(s)');
 ylabel('x(m)');
 
 %Lambda
-subplot(3,1,3)
+subplot(4,1,3)
 plot(t,z(:,3))
 xlabel('t(s)');
 ylabel('Flux Linkage(Wb-t)');
+
+%solve for fe (electrical force);
+lambda = z(:,3);
+x = z(:,2);
+i = (lambda.*x/val.k);     % Create current variable (A)
+fe = ((val.k*i.^2)/(2*x.^2));  % Electrical force
+
+%Electrical force
+subplot(4,1,4)
+plot(t,fe)
+xlabel('t(s)');
+ylabel('Electrical force(N)');
 
 % function of the system dynamic model
 function dz = G_sys(t,z,u,val)
@@ -70,7 +82,12 @@ function dz = G_sys(t,z,u,val)
     % extract external input
     f = u(1);
     Vs = u(2);
-
+    
+    %Apply voltage change at time = 0.2(S)
+    if t >= 0.2
+        Vs = 5;
+    end
+    
     % extract the states of the system
     v = z(1);       % speed of the block (m/s)
     x = z(2);       % position of the block (m)
@@ -79,9 +96,9 @@ function dz = G_sys(t,z,u,val)
     i = (lambda/(k/x)); %create current variable (A)
 
     % calculate the derivatives of the states
-    dv = 1/M*(f-((k*i^2)/(2*x^2))-K*(x-x0)-D*v);   % Acceleration (m/s^2)
+    dv = 1/M*(f- ((k*i^2)/(2*x^2)) - K*(x-x0) - D*v);   % Acceleration (m/s^2)
     dx = v;                                        % Speed (m/s)
-    dlambda = Vs+r*i;                              % Flux Linkage (Wb-t) 
+    dlambda = Vs-r*i;                              % Flux Linkage (Wb-t) 
 
     % assemble the derivative vector
     dz = [dv;dx;dlambda]; 
