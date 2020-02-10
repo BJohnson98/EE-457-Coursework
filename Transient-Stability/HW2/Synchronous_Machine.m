87ebf515d8409ccdf7a53b0b87557a1eb312d83f
Brandon Johnson <47310376+BJohnson98@users.noreply.github.com>
2020-02-09 22:27:17 -0600

    update

--- Transient-Stability/HW2/Synchronous_Machine.m
+++ Transient-Stability/HW2/Synchronous_Machine.m
@@ -25,6 +25,10 @@ VAL.Llfd = 0.0545;
 %Other variables
 VAL.D = 7;  % Damping Coefficient due to friction (N*m*s/rad)
 VAL.J = 0.0658*10^6; % Inertia of the prime mover (Kg*m^2)
+VAL.Rl = 40.8; % Resistance of the load (Ohms)
+VAL.Rfd = 0.1345;
+Val.Rs = 2.43*10^-3;
+VAL.N = 16.4 %turn ratio
 
 % initial conditions
 Lambda_as = 0;  % Flux Linkage from as
@@ -32,47 +36,62 @@ Lambda_bs = 0;  % Flux Linkage from bs
 Lambda_cs = 0;  %Flux Linkage from cs                 
 Lambda_fd = 0;  % Flux Linkage from fd              
 theta = 0;      %Initial position of the rotor (Angle)
-wr = 120*pi;         %Initial velocity of the rotor (V)
-z0 = [Lambda_as; Lambda_bs; Lambda_cs; Lambda_fd; theta; wr] 
+wr = 120*pi;    %Initial velocity of the rotor (V)
+z0 = [Lambda_as; Lambda_bs; Lambda_cs; Lambda_fd; theta; wr];
 
-% exogenous input
-Vs = 0;     % Voltage at the source (V)
-f = 0;      % Mechanical Input (N);
-u = [f;Vs]; % external force vector
 
 % simulation horizon
 T = [0 25]; % (s)
 
-
 % solve the ODE
 options = odeset('RelTol',1e-4);
-[t z] = ode15s(@(t,z)M_sys(t,z,VAL),T,z0,options);
+[t z] = ode15s(@(t,z)G_sys(t,z,VAL),T,z0,options);
 
 
+%plot the results;
 
-function dz = G_sys(t,z,val)
+% voltage
+subplot(2,1,1)
+plot(t,z(:,1))
+xlabel('t(s)');
+ylabel('Voltage(V)');
+
+% current
+subplot(5,1,2)
+plot(t,z(:,2))
+xlabel('t(s)');
+ylabel('Current(A)');
+
+function [dz y] = G_sys(t,z,VAL)
     % Extract state variables
     Lambda_as = z(1);
-    Lambda_as = z(2);
-    Lambda_as = z(3); 
-    Lambda_as = z(4); 
+    Lambda_bs = z(2);
+    Lambda_cs = z(3); 
+    Lambda_fd = z(4); 
     theta = z(5); 
     wr = z(6);
     
     % Extract other vars. 
     D = VAL.D;
     J = VAL.J;
-    
-    
-    %Inductance (H)
+    Rl = VAL.Rl;
+    Rfd = VAL.Rfd;
+    Rs = VAL.Rs;
+    % Extract Inductance (H)
     La = VAL.La;
-    %Leakage Inductance (H)
+    % Extract Leakage Inductance (H)
     Lls = VAL.Lls;
-    %Exciter Inductance (H)
+    % Extract Exciter Inductance (H)
     Lmfd = VAL.Lmfd;
     Lsfd = VAL.Lsfd;
     Llfd = VAL.Llfd;
-
+    
+    %At T = 15s change the resistance to 2.7 ohms. This simulates a load
+    %pick up event.
+    if t > 15
+        Rl = 2.7;
+    end
+    
     %Self Inductance (H)
     Lasas = La + Lls;
     Lbsbs = La + Lls;
@@ -91,11 +110,11 @@ function dz = G_sys(t,z,val)
     Inductance_Matrix = [Lasas Lasbs Lascs Lasfd; 
                          Lasbs Lbsbs Lbscs Lbsfd;
                          Lascs Lbscs Lcscs Lcsfd;
-                         Lasfd Lbsfd Lcsfd Lfdfd;]
+                         Lasfd Lbsfd Lcsfd Lfdfd;];
 
                      
     %Form flux linkage matrix.
-    Linkage = [v(1); v(2); v(3); V(4)];
+    Linkage = [Lambda_as; Lambda_bs; Lambda_cs; Lambda_fd];
     
     %solve for the current
     I = Inductance_Matrix\Linkage;
@@ -104,13 +123,25 @@ function dz = G_sys(t,z,val)
     Ics = I(3);
     Ifd = I(4);
     
+    %solve for voltage
+    Vas = -Rl*Ias - Rs*Ias;
+    Vbs = -Rl*Ibs - Rs*Ibs;
+    Vcs = -Rl*Ics - Rs*Ics;
+    Vfd = -Rl*Ifd - Rfd*Ifd;
+    
+ 
     %final differential equations.
     Te = La*Ifd*((Ias-0.5*Ibs-0.5*Ics)*cos(theta)+(Ias-0.5*Ibs-0.5*Ics)*sin(theta));
     Tf = D*wr;
     Tm = -Te+Tf;
-    dwr = 1/J*(Tm + Te -Tf); 
+    dwr = 1/J*(Tm+Te-Tf); 
     dtheta = wr;
     
-    Pe = Vas*Ias+Vbs*Ibs+Vcs*Ics+;
-
+    %Power output of the machine.
+    Pe = Vas*Ias+Vbs*Ibs+Vcs*Ics;
+    
+    dz = [Te; Tf; Tm; dwr; dtheta; Pe];
+    
 end
+
+
